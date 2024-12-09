834b36db51d1b6b5263f0d9d904c5a270c1d0cc1
alscho <alexander2791@hotmail.com>
2024-12-09 22:48:44 +0100

    added recursive pape generator, toFix: bold/italic bug, pretty up code blocks, fix quotes (in block quote)

--- src/main.py
+++ src/main.py
@@ -1,16 +1,28 @@
-from page import generate_page
+from page import generate_page, generate_page_recursively
 from files import replace_dest_with_src_copy
 
 def main():
     copy_src = "static"
     copy_dest = "public"
 
-    markdown_src = "content/index.md"
-    template_src = "template.html"
-    html_dest = "public"
-
     replace_dest_with_src_copy(copy_src, copy_dest)
 
-    generate_page(markdown_src, template_src, html_dest)
+    print("##############################\n")
 
+    single = False
+
+    if single:
+        markdown_src = "content/index.md"
+        template_src = "template.html"
+        html_dest = "public/index.html"
+
+        generate_page(markdown_src, template_src, html_dest)
+    
+    else:
+        markdown_src = "content/"
+        template_src = "template.html"
+        html_dest = "public/"
+
+        generate_page_recursively(markdown_src, template_src, html_dest)
+        
 main()
--- src/page.py
+++ src/page.py
@@ -1,16 +1,33 @@
 import os
+import re
 from markdown import markdown_to_html_node, extract_title
+
+def generate_page_recursively(dir_path_content, template_path, dest_dir_path):
+    contents = os.listdir(dir_path_content)
+    for content in contents:
+        content_path = os.path.join(dir_path_content, content)
+        #print(f"content_path: {content_path}")
+        if os.path.isfile(content_path):
+            #print(f"content_path: {content_path}")
+            dest_path = str(os.path.join(dest_dir_path, content))
+            dest_path = dest_path.replace(".md", ".html")
+            #print(f"dest_path: {dest_path}")
+            generate_page(content_path, template_path, dest_path)
+        else:
+            dest_path = os.path.join(dest_dir_path, content)
+            generate_page_recursively(content_path, template_path, dest_path)
+
 
 def generate_page(from_path, template_path, dest_path):
 
     ##### hardcoded
     placeholders = {"title": "{{ Title }}", "content": "{{ Content }}"}
-    html_name = "index.html"
+    #html_name = "index.html"
     #####
 
-    html_path = os.path.join(dest_path, html_name)
+    #html_path = os.path.join(dest_path, html_name)
 
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}.")
+    print(f"Generating page from '{from_path}' to '{dest_path}' using '{template_path}'.")
     
     from_file = read_savely(from_path)
     template_file = read_savely(template_path)
@@ -28,14 +45,18 @@ def generate_page(from_path, template_path, dest_path):
 
     print(f"Created *.html")
 
-    os.makedirs(dest_path, exist_ok = True)
+    ### not pretty, but works - drops the file name from dest_path
+    temp3 = dest_path.rsplit("/", 1)
+    dest_dir = temp3[0]
+    #print(f"dest_dir: {dest_dir}")
+    os.makedirs(dest_dir, exist_ok = True)
 
     print(f"Created destination filepath if neccesary.")
 
-    with open(html_path, "w+") as f:
+    with open(dest_path, "w+") as f:
         f.write(html_page)
     
-    print(f"Page '{html_page}' created from '{from_path}' using '{template_path}'.\n")
+    print(f"Page '{dest_path}' created from '{from_path}' using '{template_path}'.\n")
 
 def read_savely(path):
     with open(path) as f:
